c850241a02921debcaa55de65aa1e1df7254ce0f
antepolu <161872440+jqmxaec@users.noreply.github.com>
2024-03-07 19:41:47 +0300

    update readme

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import find_packages
 setup(
     name="madotsuki",
     version="1.0",
-    description="Библиотека для упрощения построения CRUD-интерфейсов",
+    description="Библиотека для построения графических CRUD-интерфейсов",
     author="jqmxaec",
     packages=find_packages(),
     install_requires=["PyQt5", "SQLAlchemy"],
